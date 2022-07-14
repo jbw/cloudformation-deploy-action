40deef357596c0b88ce6ef53f378142768e93097
Jason Watson <jwatson@breachquest.com>
2022-07-14 09:57:00 +0100

    refactor: run the script

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import * as core from "@actions/core";
 
 // Create github action
 export async function run() {
-  try {
-    const name = core.getInput("name");
-    core.setOutput("name", name);
-  } catch (error: any) {
-    core.setFailed(error.message);
+  const name = core.getInput("message");
+  if (name) {
+    core.debug(`Hello ${name}!`);
+
+    return core.setOutput("outputMessage", name);
   }
+
+  core.setFailed("No message provided");
 }
+
+run();
