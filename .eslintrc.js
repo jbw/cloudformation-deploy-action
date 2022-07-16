a07e814769537a0130f5c1a94b879e1b2fce75a7
Jason Watson <jwatson@breachquest.com>
2022-07-16 22:16:24 +0100

    chore: update lint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,26 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "tsconfig.json",
-    sourceType: "module",
+    project: 'tsconfig.json',
+    sourceType: 'module',
     ecmaVersion: 2020,
   },
   env: {
     node: true,
     jest: true,
   },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'no-console': 'warn',
+    'require-await': 'off',
+    'no-return-await': 'off',
+    '@typescript-eslint/return-await': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-expressions': 'error',
+  },
 };
