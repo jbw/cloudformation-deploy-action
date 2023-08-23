77fe55d87013d3576f235ea5eae06be42e757f8f
Jason Watson <jwatson@breachquest.com>
2023-08-23 18:01:31 +0100

    test: dont wait

--- test/deploy.spec.ts
+++ test/deploy.spec.ts
@@ -39,7 +39,7 @@ describe('deploy', () => {
       stack: {
         name: stackName,
         template: { filepath: 'test/test-template.json' },
-        waitFor: true,
+        waitFor: false,
         deleteFailedChangeSet: false,
         parameterOverrides: [
           {
